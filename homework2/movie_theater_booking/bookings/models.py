6714a28f605171d1af3865b28dd94d46bae48cbf
JeffreyKotz <unpredictabledear@gmail.com>
2026-02-20 23:58:44 +0000

    Implemented Movie, Seat, Booking __str__ methods and added docstrings

--- homework2/movie_theater_booking/bookings/models.py
+++ homework2/movie_theater_booking/bookings/models.py
@@ -1,27 +1,41 @@
+"""
+By Jeffrey Kotz - 2/20/2026
+Models representing Movie, Seat, and Booking for the Movie Theater Booking Application
+"""
+
 from django.db import models
 
 
-# Create your models here.
 class Movie(models.Model):
     """Model representing a movie
-
-    Args:
-        models (Model): used to specify data type of fields for model
     """
-    title = models.CharField()
-    description = models.CharField()
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=400)
     release_date = models.DateTimeField("release date")
     duration = models.DurationField()
+
+    def __str__(self) -> str:
+        """Return human readable representation of the seat
+
+        Returns:
+            str: human readable description of seat
+        """
+        return self.title
 
 
 class Seat(models.Model):
     """Model representing a seat
-
-    Args:
-        models (Model): used to specify data type of fields for model
     """
-    seat_number = models.IntegerField()
-    booking_status = models.BooleanField()
+    seat_number = models.IntegerField(default=0)
+    booking_status = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        """Return human readable representation of the seat
+
+        Returns:
+            str: human readable description of seat
+        """
+        return f"seat #{self.seat_number}, Booked: {self.booking_status}"
 
 
 class Booking(models.Model):
@@ -30,6 +44,19 @@ class Booking(models.Model):
     Args:
         models (Model): used to specify data type of fields for model
     """
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
+    # OneToOneField allows the type of the field to be specified to another.
+    # on_delete=model.CASCADE indicates that when deleted, the object
+    # containing the OneToOneField is also deleted.
+    movie = models.OneToOneField(Movie, on_delete=models.CASCADE)
+    seat = models.OneToOneField(Seat, on_delete=models.CASCADE)
     booking_date = models.DateTimeField("booking date")
+
+    def __str__(self) -> str:
+        """Return human readable representation of the booking
+
+        Returns:
+            str: human readable description of booking
+        """
+        return (f"Booking for movie: {self.movie}, "
+                f"in {self.seat}, "
+                f"on {self.booking_date}")
